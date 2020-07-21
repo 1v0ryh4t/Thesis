ec4461992b747443a5529e781759dc4291fea81a
Erin Rutan <erin.rutan.17@cnu.edu>
2020-07-20 23:12:40 -0400

    Added Unity Code

---
+++ HoloHelloWorld/Assets/TanGameObjects/TanScripts/DisplayLocation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayLocation : MonoBehaviour
+{
+    public GameObject keyboard;
+    public TextMesh display;
+
+    // Update is called once per frame
+    void Update()
+    {
+        display.text = keyboard.transform.position.ToString();
+    }
+}
